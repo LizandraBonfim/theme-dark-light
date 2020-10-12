7c83bf3a292f12e9dee333fbdc684ba3b7b41a5c
LizandraBonfim <lizandra.oliveirasouza@hotmail.com>
2020-10-11 22:41:33 -0300

    Updates

---
+++ precache-manifest.1cee48a1d2bf5b132ae94f136238cb6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebc74d249a7586963fe98bbf4e08576",
+    "url": "/theme-dark-light/index.html"
+  },
+  {
+    "revision": "2122dd1ee5d870fd3b62",
+    "url": "/theme-dark-light/static/js/2.8f648c2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/theme-dark-light/static/js/2.8f648c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000b29509b290adcff67",
+    "url": "/theme-dark-light/static/js/main.22cbb61a.chunk.js"
+  },
+  {
+    "revision": "0bacf974536d3aed4f57",
+    "url": "/theme-dark-light/static/js/runtime-main.5cd8bf1b.js"
+  }
+]);
